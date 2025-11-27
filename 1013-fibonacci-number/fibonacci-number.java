917f957806d2d263c34f999cbae1db673e4bdf0f
rajan78870-cell <rajan78870@gmail.com>
2025-11-27 15:25:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (38.03%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,18 +1,22 @@
-class Solution {
-    public int fibonicci(int n , int[] dp) {
-        if(n<=1){
-            return n;
-        }
-        if (dp [n] != -1)
-        return dp [n];
+class Solution { 
 
-        return dp[n] = fibonicci(n-1,dp) + fibonicci(n-2,dp);
+    int fibonicci(int n, int[] dp) {
+
+        if(n <= 1) return n;
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = fibonicci(n - 1, dp) + fibonicci(n - 2, dp);
     }
-    public int fib(int n){
-        int[] dp = new int[n+1];
-        for (int i = 0 ; i <= n; i ++){
-            dp [i]= -1;
+
+    public int fib(int n) {
+
+        int[] dp = new int[n + 1];
+
+        for(int i = 0; i <= n; i++){
+            dp[i] = -1;
         }
-        return fibonicci(n,dp);
+
+        return fibonicci(n, dp);
     }
 }
