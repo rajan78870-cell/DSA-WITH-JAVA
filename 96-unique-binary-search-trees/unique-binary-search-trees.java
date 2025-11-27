9167d2eea432f37bf1c9b53480667580ea5c6603
rajan78870-cell <rajan78870@gmail.com>
2025-11-27 13:26:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (29.61%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+
+        // Base Cases
+        dp[0] = 1;  // Empty tree
+        dp[1] = 1;
+
+        // Build DP table
+        for (int nodes = 2; nodes <= n; nodes++) {
+            for (int root = 1; root <= nodes; root++) {
+                dp[nodes] += dp[root - 1] * dp[nodes - root];
+            }
+        }
+
+        return dp[n];
+    }
+}
