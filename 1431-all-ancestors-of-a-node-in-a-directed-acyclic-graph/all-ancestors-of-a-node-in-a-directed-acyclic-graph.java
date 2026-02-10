f238b8e04d9e4fc3f2bb0e178b5408dda74f78a2
Rajan Yadav <rajan78870@gmail.com>
2026-02-10 12:49:52 +0530

    Time: 90 ms (44.44%) | Memory: 100.6 MB (37.24%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-        // Graph adjacency list
+       
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        // Indegree array
+       
         int[] indegree = new int[n];
 
-        // Build graph and indegree
+        
         for (int[] e : edges) {
             int u = e[0], v = e[1];
             graph.get(u).add(v);
             indegree[v]++;
         }
 
-        // Each node keeps a set of ancestors
+      
         List<Set<Integer>> ancestors = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             ancestors.add(new HashSet<>());
         }
 
-        // Topological sort (Kahn's Algorithm)
+       
         Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             if (indegree[i] == 0) q.offer(i);
@@ -34,9 +34,9 @@ class Solution {
             int u = q.poll();
 
             for (int v : graph.get(u)) {
-                // Add u as ancestor of v
+                
                 ancestors.get(v).add(u);
-                // Add all ancestors of u to v
+                
                 ancestors.get(v).addAll(ancestors.get(u));
 
                 indegree[v]--;
@@ -46,7 +46,7 @@ class Solution {
             }
         }
 
-        // Convert sets to sorted lists
+        
         List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             List<Integer> list = new ArrayList<>(ancestors.get(i));
