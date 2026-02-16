a9547b93a8932ad7f90eff2bac074e942d50fe82
Rajan Yadav <rajan78870@gmail.com>
2026-02-16 17:13:58 +0530

    Time: 1 ms (98.43%) | Memory: 46.6 MB (97.57%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) return false;
+        if (match(head, root)) return true;
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean match(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null) return false;
+        if (head.val != root.val) return false;
+        return match(head.next, root.left) || match(head.next, root.right);
+    }
+}
